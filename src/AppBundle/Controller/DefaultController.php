7df3974ea7e80d7fb3ab4bd662acd915ba2fc2e6
Abdelkarim EL AMEL <aelamel@octo.com>
2019-05-09 18:37:47 +0000

    Displaying redis keys

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,26 @@
 
 namespace AppBundle\Controller;
 
+use Snc\RedisBundle\Client\Phpredis\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+    /** @var  Client */
+    private $redisClient;
+
+    public function __construct(Client $client)
+    {
+        $this->redisClient = $client;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $redisKeys = $this->redisClient->get('*');
+        return $this->json(['keys' => $redisKeys]);
     }
 }
